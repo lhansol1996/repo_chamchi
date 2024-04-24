aa9556863846659f708e30168ce6d9c447d7f193
gksqhfdl12 <gksqhfdl12@gmail.com>
2024-04-24 17:37:22 +0900

    merge 2024 0424 1737

--- chamchi/src/main/java/com/ERR/common/config/WebMvcConfigurerImpl.java
+++ chamchi/src/main/java/com/ERR/common/config/WebMvcConfigurerImpl.java
@@ -13,13 +13,13 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer{
 		public void addInterceptors(InterceptorRegistry registry) {
 			
 			registry.addInterceptor(new CheckLoginSessionInterceptor()) //LoginCheckInterceptor 등록
-					.addPathPatterns("/**Xdm**") // 로그인없이는 전체경로 다막음
+					.addPathPatterns("/*Xdm*") // 로그인없이는 전체경로 다막음
 					
 					.excludePathPatterns(	//예외 경로 
 							"xdm/**",
 							"usr/**",
-							"index/**"   // static 경로 (css 망가짐방지를 위한 예외설정)
-							
+							"index/**",   // static 경로 (css 망가짐방지를 위한 예외설정)
+							"/adminMemberLogin"
 							
 					)
 			;
